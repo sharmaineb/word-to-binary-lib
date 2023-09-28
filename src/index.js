75283faeaf0c5f302b074cdc0c94f9ce56b64c18
Sharmaine <sharmaine.borbe@students.dominican.edu>
2023-09-28 15:50:57 -0700

    [update]

--- src/index.js
+++ src/index.js
@@ -1,27 +1,38 @@
-function wordToBinary(word) {
-    const mapping = {
-      'a': '01100001', 'b': '01100010', 'c': '01100011', 'd': '01100100', 'e': '01100101',
-      'f': '01100110', 'g': '01100111', 'h': '01101000', 'i': '01101001', 'j': '01101010',
-      'k': '01101011', 'l': '01101100', 'm': '01101101', 'n': '01101110', 'o': '01101111',
-      'p': '01110000', 'q': '01110001', 'r': '01110010', 's': '01110011', 't': '01110100',
-      'u': '01110101', 'v': '01110110', 'w': '01110111', 'x': '01111000', 'y': '01111001',
-      'z': '01111010'
-    };
+// character to Binary Mapping
+const charToBinary = {
+    'a': '01100001', 'b': '01100010', 'c': '01100011', 'd': '01100100',
+    'e': '01100101', 'f': '01100110', 'g': '01100111', 'h': '01101000',
+    'i': '01101001', 'j': '01101010', 'k': '01101011', 'l': '01101100',
+    'm': '01101101', 'n': '01101110', 'o': '01101111', 'p': '01110000',
+    'q': '01110001', 'r': '01110010', 's': '01110011', 't': '01110100',
+    'u': '01110101', 'v': '01110110', 'w': '01110111', 'x': '01111000',
+    'y': '01111001', 'z': '01111010',
+  };
   
-    let binary = '';
-    for (const char of word.toLowerCase()) { // convert char to lowercase
-      if (char in mapping) {
-        binary += mapping[char] + ' ';
-      } else {
-        binary += char + ' (not in mapping) ';
-      }
+  // helper function to convert a character to binary or '(not in mapping)' if not found
+  function charToBinaryOrNot(char) {
+    if (char === ' ') {
+      return ' '; // save spaces
     }
-  
-    return binary.trim();
+    const lowerChar = char.toLowerCase(); // convert to lowercase for mapping 
+    if (charToBinary[lowerChar]) {
+      return charToBinary[lowerChar];
+    } else {
+      return char.charCodeAt(0).toString(2).padStart(8, '0') + ` (${char} not in mapping)`;
+    }
   }
   
+  // function to convert a word to binary
+  function wordToBinary(word) {
+    const binaryArray = word.split('').map(charToBinaryOrNot);
+    return binaryArray.join(' ');
+  }
   
-  module.exports = { wordToBinary };
+  module.exports = {
+    wordToBinary,
+  };
+  
+  
   
   
   
--- tests/test.js
+++ tests/test.js
@@ -1,35 +1,24 @@
 const { wordToBinary } = require('../src/index');
 
-describe('wordToBinary', () => {
-  it('should convert a word to binary', () => {
+test('Converts a word to binary', () => {
     const binary = wordToBinary('hello');
     expect(binary).toBe('01101000 01100101 01101100 01101100 01101111');
   });
-
-  it('should handle mixed-case input', () => {
+  
+  test('Handles mixed-case input', () => {
     const binary = wordToBinary('HeLlO');
     expect(binary).toBe('01101000 01100101 01101100 01101100 01101111');
   });
-
-  it('should handle characters not in the mapping', () => {
+  
+  test('Handles characters not in the mapping', () => {
     const binary = wordToBinary('word$'); // $ is not in the mapping
-    expect(binary).toBe('01110111 01101111 01110010 01100100 $ (not in mapping)');
+    expect(binary).toBe('01110111 01101111 01110010 01100100 ($ not in mapping)');
   });
-
-  it('should handle the entire lowercase alphabet', () => {
-    const alphabet = 'abcdefghijklmnopqrstuvwxyz';
-    const binary = wordToBinary(alphabet);
-    const expectedBinary = alphabet
-      .split('')
-      .map((char) => char.charCodeAt(0).toString(2).padStart(8, '0'))
-      .join(' ');
-  
-    expect(binary).toBe(expectedBinary);
+    
+  test('Handles an empty word', () => {
+    const binary = wordToBinary('');
+    expect(binary).toBe('');
   });
-  
-  
-  
-});
 
 
   
